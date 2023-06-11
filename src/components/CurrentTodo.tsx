9fe6bd06b7c8189e6f6def8424e1fa11ecbd6f73
the-dmar <thedmart22@gmail.com>
2023-06-11 10:43:40 -0400

    Add function to save todos

--- src/components/CurrentTodo.tsx
+++ src/components/CurrentTodo.tsx
@@ -4,18 +4,25 @@ interface CurrentTodoProps {
     created: Date
   }
   editCurrentTodo: (newText: string) => void
+  todoInputRef: React.MutableRefObject<HTMLInputElement | null>
+  saveCurrentTodo: (key?: string) => void
 }
 
 export default function CurrentTodo({
   currentTodo,
   editCurrentTodo,
+  todoInputRef,
+  saveCurrentTodo,
 }: CurrentTodoProps) {
   return (
     <div>
       <input
         onChange={e => editCurrentTodo(e.target.value)}
         value={currentTodo.text}
+        ref={todoInputRef}
+        onKeyDown={e => saveCurrentTodo(e.key)}
       />
+      <button onClick={() => saveCurrentTodo()}>Save Todo</button>
     </div>
   )
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,14 +1,38 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import CurrentTodo from "./CurrentTodo"
+import Todo from "../types/todo"
 
 export default function Todos() {
+  const [todos, setTodos] = useState<Todo[]>([])
   const [currentTodo, setCurrentTodo] = useState({
     text: "",
     created: new Date(),
   })
 
+  const todoInputRef = useRef<HTMLInputElement | null>(null)
+
   function editCurrentTodo(newText: string) {
     setCurrentTodo(currentTodo => ({ ...currentTodo, text: newText }))
+  }
+
+  function saveCurrentTodo(key?: string) {
+    if (!key || key === "Enter") {
+      const existingTodoIndex = todos.findIndex(
+        ({ created }) => created === currentTodo.created
+      )
+
+      if (existingTodoIndex >= 0) {
+        setTodos(todos =>
+          todos.map(todo =>
+            todo.created === currentTodo.created ? currentTodo : todo
+          )
+        )
+      } else {
+        setTodos(todos => [...todos, currentTodo])
+        todoInputRef.current?.focus()
+      }
+      setCurrentTodo({ text: "", created: new Date() })
+    }
   }
 
   return (
@@ -16,8 +40,11 @@ export default function Todos() {
       <CurrentTodo
         currentTodo={currentTodo}
         editCurrentTodo={editCurrentTodo}
+        todoInputRef={todoInputRef}
+        saveCurrentTodo={saveCurrentTodo}
       />
       <pre>{JSON.stringify(currentTodo, null, 2)}</pre>
+      <pre>{JSON.stringify(todos, null, 2)}</pre>
     </section>
   )
 }
